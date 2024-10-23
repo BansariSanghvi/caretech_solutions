211a676f6d8511f097379415bef8e2ad3b426adf
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2024-10-23 15:31:47 +0100

    Connected the Medical Inventory Page to sidebar and added the template code onto this page

--- common/sidebar.php
+++ common/sidebar.php
@@ -6,7 +6,7 @@
             <div class="items">
                 <li><i class='bx bxs-home'></i><a href="index.php">Dashboard</a></li>
                 <li><i class='bx bxs-user-detail'></i><a href="staff_hub.php">Staff Hub</a></li>
-                <li><i class='bx bxs-capsule'></i><a href="#">Medicines</a></li>
+                <li><i class='bx bxs-capsule'></i><a href="medicineList.php">Medicines</a></li>
                 <li><i class='bx bxs-buildings'></i><a href="#">GP Surgery / Hospital</a></li>
                 <li><i class='bx bx-line-chart'></i><a href="#">Analytics</a></li>
                 <li><i class='bx bxs-cog'></i><a href="#">Settings</a></li>

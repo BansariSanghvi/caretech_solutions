5e9091fd242d0293588e69fc2a4aa2b43848d6b0
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2024-10-23 15:24:18 +0100

    created the inner template code for the Staff_Hub page

--- common/sidebar.php
+++ common/sidebar.php
@@ -1,7 +1,7 @@
 <section id="menu">
             <div class="logo">
                 <img src="img/heart.png" alt="">
-                <h2 class="name">CareTech</h2>
+                <h2 class="name">CareTech Ltd</h2>
             </div>
             <div class="items">
                 <li><i class='bx bxs-home'></i><a href="index.php">Dashboard</a></li>

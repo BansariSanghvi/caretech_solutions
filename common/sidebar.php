c69e08052659f4edea88efed0e7a4431df2bac72
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2024-10-23 15:38:18 +0100

    Updated the sidebar with the connected file for medical branches

--- common/sidebar.php
+++ common/sidebar.php
@@ -7,7 +7,7 @@
                 <li><i class='bx bxs-home'></i><a href="index.php">Dashboard</a></li>
                 <li><i class='bx bxs-user-detail'></i><a href="staff_hub.php">Staff Hub</a></li>
                 <li><i class='bx bxs-capsule'></i><a href="medicineList.php">Medicines</a></li>
-                <li><i class='bx bxs-buildings'></i><a href="#">GP Surgery / Hospital</a></li>
+                <li><i class='bx bxs-buildings'></i><a href="medicalBranches.php">GP Surgery / Hospital</a></li>
                 <li><i class='bx bx-line-chart'></i><a href="#">Analytics</a></li>
                 <li><i class='bx bxs-cog'></i><a href="#">Settings</a></li>
                 <li><i class="ri-alert-fill"></i><a href="#">Report Problem</a></li>

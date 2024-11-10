f7eb018ecb1a55de9e14ea1d662912f301aa16b5
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2024-11-10 12:40:59 +0000

    Created and linked the adding medical branches functionality

--- medicalBranches.php
+++ medicalBranches.php
@@ -127,7 +127,7 @@
                     <div class="staff_hub_top_container">
                         <div class="buttons-container">
                         <div class="add_staff_box">
-                            <button class = "add_staff_btn" onclick="window.location.href='add_staff_form.php'"><i class="ri-user-add-line"></i>   Add Branch</button>
+                            <button class = "add_staff_btn" onclick="window.location.href='add_branch.php'"><i class="ri-user-add-line"></i>   Add Branch</button>
                         </div>
                     <div class="remove_staff_box">
                         <button class="remove_staff_btn"><i class='bx bxs-minus-square'></i>  Remove Branch</button>

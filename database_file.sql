d1b8fe2b54cb5b5acbb708cb103777f6d3b147e5
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2024-12-06 13:34:03 +0000

    Updated the Database so that admin has branch to be associated to and staff has it's own login

--- database_file.sql
+++ database_file.sql
@@ -278,6 +278,7 @@ INSERT INTO hospital_branches (department_name, department_email, department_typ
 ('Orthopedics', 'orthopedics@cityhospital.com', 'Surgical', '01512345679', 1),
 ('Rehabilitation', 'rehabilitation@cityhospital.com', 'Rehabilitation', '01173216548', 1),
 ('General Medicine', 'general.medicine@cityhospital.com', 'Medical', '01865782347', 1);
+('Adminstration','admin@caretech.com','Admin','1234689300','1');
 
 
 INSERT INTO external_associations (medical_association_name, associations_location, associations_phone, associations_email, hospital_id) VALUES

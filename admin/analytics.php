a4d8935998ce0afc7f0f590b14714380b458dc64
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2024-12-06 14:40:14 +0000

    Connected Admin Analytics Page

--- admin/analytics.php
+++ admin/analytics.php
@@ -29,7 +29,7 @@
 
                     <!-------Add the other elements under this------>
 
-                    <h3 class="title" id="test">Hello World</h3>
+                    
 
                   </section>
                   

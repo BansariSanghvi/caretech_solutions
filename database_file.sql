7f901cd112b27ed3f86c628235c4cec7f468efb3
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2024-12-17 22:49:24 +0000

    Updated the Database file with the priority catagory.

--- database_file.sql
+++ database_file.sql
@@ -2,38 +2,40 @@
 
 /* Table for hospital information */
 CREATE TABLE hospital_info (
-    hospital_id INT PRIMARY KEY AUTO_INCREMENT,
-    hname VARCHAR(255) NOT NULL,
-    hospital_address VARCHAR(255) NOT NULL,
-    hospital_phone CHAR(11) NOT NULL,
-    hospital_email VARCHAR(255) NOT NULL
+    hospital_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    hname VARCHAR(255),
+    hospital_address VARCHAR(255),
+    hospital_phone CHAR(11),
+    hospital_email VARCHAR(255)
 );
 
 /* Table for staff records within the hospital */
 CREATE TABLE staff_records (
-    staff_id INT PRIMARY KEY AUTO_INCREMENT,
-    fname VARCHAR(20) NOT NULL,
-    lname VARCHAR(20) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    staff_phone_no CHAR(10) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    role VARCHAR(255) NOT NULL,
-    hospital_department_id INT NOT NULL,
-    hospital_id INT NOT NULL,
-    isActive BOOLEAN DEFAULT TRUE,
+    staff_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    fname VARCHAR(20),
+    lname VARCHAR(20),
+    address VARCHAR(255),
+    staff_phone_no CHAR(10),
+    email VARCHAR(255),
+    role VARCHAR(255),
+    hospital_department_id INT(10),
+    hospital_id INT(10),
+    isActive BOOLEAN,  
     reasonToLeave VARCHAR(255) NULL,
+
+
     FOREIGN KEY (hospital_department_id) REFERENCES hospital_branches(hospital_department_id),
     FOREIGN KEY (hospital_id) REFERENCES hospital_info(hospital_id)
 );
 
 /* Table for patient records */
 CREATE TABLE patient_records (
-    patient_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    phone_no CHAR(11) NOT NULL,
-    date_of_birth INT(8),
+    patient_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    email VARCHAR(50),
+    phone_no CHAR(11),
+    date_of_birth INT(8)
     emergency_contact CHAR(10),
     emergency_contact_name VARCHAR(255),
     patient_history VARCHAR(255),
@@ -41,7 +43,8 @@ CREATE TABLE patient_records (
     staff_id INT(10),
     hospital_id INT(10),
     medical_association_id INT(10),
-    last_seen_date INT(8),
+    last_seen_date INT(8),  
+
     FOREIGN KEY (staff_id) REFERENCES staff_records(staff_id),
     FOREIGN KEY (hospital_id) REFERENCES hospital_info(hospital_id),
     FOREIGN KEY (medical_association_id) REFERENCES external_associations(medical_association_id)
@@ -49,31 +52,35 @@ CREATE TABLE patient_records (
 
 /* Table for hospital departments (branches) */
 CREATE TABLE hospital_branches (
-    hospital_department_id INT PRIMARY KEY AUTO_INCREMENT,
+    hospital_department_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(255),
     department_email VARCHAR(255),
     department_type VARCHAR(255), -- Department Type
     department_phone CHAR(11),
     hospital_id INT(10),
+
     FOREIGN KEY (hospital_id) REFERENCES hospital_info(hospital_id)
 );
 
 /* Table for external medical associations such as GP Surgery */
+-- Maybe I need Catagory onto this? Primary Care 
 CREATE TABLE external_associations (
-    medical_association_id INT PRIMARY KEY AUTO_INCREMENT,
+    medical_association_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     medical_association_name VARCHAR(25),
     associations_location VARCHAR(255),
     associations_phone CHAR(11),
     associations_email VARCHAR(255),
-    hospital_id INT(10),
+    hospital_id INT(10), -- main hosptial branch
+
     FOREIGN KEY (hospital_id) REFERENCES hospital_info(hospital_id)
 );
 
 /* Table for referral forms */
 CREATE TABLE referal_form (
-    request_id INT PRIMARY KEY AUTO_INCREMENT,
+    request_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     request_type VARCHAR(255),
     summary_notes VARCHAR(255),
+    priority_catagory VARCHAR(255),
     hospital_department_id INT(10),
     staff_id INT(10),
     hospital_id INT(10),
@@ -87,17 +94,21 @@ CREATE TABLE referal_form (
     FOREIGN KEY (medical_association_id) REFERENCES external_associations(medical_association_id)
 );
 
+
+
+
 /* Table for prescription orders */
 CREATE TABLE prescription_order (
-    prescription_id INT PRIMARY KEY AUTO_INCREMENT,
+    prescription_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     patient_id INT(10),
     hospital_id INT(10),
     to_address VARCHAR(255),
     from_address VARCHAR(255),
-    date_issued INT(8),
+    date_issued INT(8), 
     is_repeat BOOLEAN,
     is_NHS_covered BOOLEAN,
     order_status VARCHAR(20) DEFAULT 'Pending',
+
     FOREIGN KEY (hospital_id) REFERENCES hospital_info(hospital_id),
     FOREIGN KEY (patient_id) REFERENCES patient_records(patient_id)
 );
@@ -111,6 +122,7 @@ CREATE TABLE prescription_items (
     frequency VARCHAR(50) NOT NULL,
     duration VARCHAR(50) NOT NULL,
     notes VARCHAR(255),
+
     FOREIGN KEY (drugID) REFERENCES drugs_list(drugID),
     FOREIGN KEY (prescription_id) REFERENCES prescription_order(prescription_id)
 );
@@ -123,6 +135,7 @@ CREATE TABLE drugs_list (
     description VARCHAR(255),
     qty INT(10),
     price INT(10),
+
     FOREIGN KEY (supplier_id) REFERENCES manufacturers(supplier_id)
 );
 
@@ -135,18 +148,20 @@ CREATE TABLE medicalEquipment_list (
     price INT(10),
     isUrgent BOOLEAN,
     hospital_department_id INT(10),
-    FOREIGN KEY (hospital_department_id) REFERENCES hospital_branches(hospital_department_id)
+
+    FOREIGN KEY (hospital_department_id) REFERENCES hospital_branches(hospital_department_id),
 );
 
 /* Table for medical equipment orders */
 CREATE TABLE equipment_orders (
-    order_number INT PRIMARY KEY AUTO_INCREMENT,
+    order_number INT PRIMARY KEY AUTO_INCREMENT, 
     equipment_ID INT(10),
     order_qty INT(10),
-    order_date INT(8),
+    order_date INT(8),  
     hospital_department_id INT(10),
     delivery_status VARCHAR(20) DEFAULT 'Pending',
     supplier_id INT(10),
+
     FOREIGN KEY (equipment_ID) REFERENCES medicalEquipment_list(equipment_ID),
     FOREIGN KEY (hospital_department_id) REFERENCES hospital_branches(hospital_department_id),
     FOREIGN KEY (supplier_id) REFERENCES manufacturers(supplier_id)
@@ -155,10 +170,11 @@ CREATE TABLE equipment_orders (
 /* Table for generating referral letters */
 CREATE TABLE referal_letters (
     letter_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    date_generated INT(8),
+    date_generated INT(8),  
     hospital_department_id INT(10),
     staff_id INT(10),
     patient_id INT(10),
+
     FOREIGN KEY (hospital_department_id) REFERENCES hospital_branches(hospital_department_id),
     FOREIGN KEY (staff_id) REFERENCES staff_records(staff_id),
     FOREIGN KEY (patient_id) REFERENCES patient_records(patient_id)
@@ -170,14 +186,15 @@ CREATE TABLE appointments (
     patient_id INT(10),
     hospital_department_id INT(10),
     staff_id INT(10),
-    appointment_date DATE,
-    appointment_time TIME,
+    appointment_date DATE, 
+    appointment_time TIME,  
+
     FOREIGN KEY (patient_id) REFERENCES patient_records(patient_id),
     FOREIGN KEY (hospital_department_id) REFERENCES hospital_branches(hospital_department_id),
     FOREIGN KEY (staff_id) REFERENCES staff_records(staff_id)
 );
 
-/* Manufacturer Table - only for medical equipment */
+/* Manufacturer Table - only for medical equipment  */
 CREATE TABLE manufacturers (
     supplier_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     supplier_name VARCHAR(255),
@@ -189,7 +206,7 @@ CREATE TABLE manufacturers (
 
 /* Announcements Table */
 CREATE TABLE annoucments (
-    annoucment_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    annoucment_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     annoucment_duration INT(100),
     annoucment_description VARCHAR(255)
 );
@@ -204,6 +221,8 @@ CREATE TABLE users (
     user_level_id INT(3),
     hospital_department_id INT(10) NULL,
     external_associations_id INT(10) NULL,
+
+
     FOREIGN KEY (user_level_id) REFERENCES user_level(user_level_id)
 );
 
@@ -216,20 +235,22 @@ CREATE TABLE user_level (
 
 /* Approvals Table */
 CREATE TABLE approvals (
-    approval_id INT PRIMARY KEY AUTO_INCREMENT,
+    approval_id INT PRIMARY KEY AUTO_INCREMENT, 
     user_id INT(10),
-    hospital_department_id INT(10),
+    hospital_department_id INT(10), 
     equipment_ID INT(10),
     approval_qty INT(10),
     approval_description VARCHAR(255),
-    approval_sent_date DATE DEFAULT CURRENT_TIMESTAMP,
-    approval_date INT(8) NULL,
+    approval_sent_date DATE CURRENT_TIMESTAMP,
+    approval_date INT(8) NULL, 
     approval_status VARCHAR(255) DEFAULT 'Waiting Approval',
+     
+
     FOREIGN KEY (equipment_ID) REFERENCES medicalEquipment_list(equipment_ID),
     FOREIGN KEY (hospital_department_id) REFERENCES hospital_branches(hospital_department_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+    
+)
 
 /* SECTION 2 - INSERTION */ 
 
@@ -273,12 +294,14 @@ INSERT INTO users (user_type, user_name, user_email, user_password, user_level_i
 ('Admin', 'admin_user', 'admin@caretech.com', 'admin123', 1, NULL ,NULL),
 ('Manager', 'manager', 'manager@caretech.com', 'manager123', 2,2,NULL),
 ('GP', 'gp_staff', 'gp@caretech.com', 'gp123', 3,NULL, 1);
+('HStaff', 'staff', 'staff@caretech.com', 'staff123', 3,NULL, 1);
 
 
 INSERT INTO user_level (user_type, description) VALUES
 ('Admin', 'Administrator with full access'),
 ('Manager', 'Hospital manager with access to hospital data and settings'),
 ('GP',' External General Practitioner');
+('Staff', "Member of Staff of Specific Department" )
 
 INSERT INTO manufacturers (supplier_name,supplier_email, supplier_location, supplier_phone, supplier_status) VALUES
 ('MedSupplies Ltd', 'info@medsupplies.com', 'London, UK', '02079460001', 'Active'),
@@ -294,7 +317,7 @@ INSERT INTO drugs_list (drugName, supplier_id, description, qty, price) VALUES
 ('Paracetamol', 4, 'Painkiller used for mild to moderate pain relief', 250, 5),
 ('Lisinopril', 5, 'ACE inhibitor used for high blood pressure treatment', 120, 30);
 
-INSERT INTO medicalEquipment_list (`equipment_ID`, `equipment_Name`, `equipment_description`, `qty`, `price`, `isUrgent`, `hospital_department_id`) VALUES
+INSERT INTO `medicalequipment_list` (`equipment_ID`, `equipment_Name`, `equipment_description`, `qty`, `price`, `isUrgent`, `hospital_department_id`) VALUES
 (1, 'ECG Machine', 'Used for measuring the electrical activity of the heart.', 5, 12000, 1, 1),
 (2, 'Defibrillator', 'Essential for emergency cardiac situations.', 25, 15000, 1, 2),
 (3, 'Orthopedic Drill', 'Used in surgical orthopedic procedures.', 2, 8000, 0, 3),

9288236f4947a25f375502f17f8bf88917911040
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2025-01-07 17:13:28 +0000

    Updated Branch Sidebar.

--- common/branch_sidebar.php
+++ common/branch_sidebar.php
@@ -4,22 +4,22 @@
         <h3 class="name">CareTech Solutions</h3>
     </div>
     <div class="items">
-        <li class="<?php echo ($current_page == 'dashboard') ? 'active' : ''; ?>">
+        <li>
             <i class='bx bxs-home'></i><a href="branchDashboard.php">Dashboard</a>
         </li>
-        <li class="<?php echo ($current_page == 'staff') ? 'active' : ''; ?>">
+        <li>
             <i class='bx bxs-user-detail'></i><a href="branchStaff.php">Staff Hub</a>
         </li>
-        <li class="<?php echo ($current_page == 'patients') ? 'active' : ''; ?>">
+        <li>
             <i class='bx bx-male-female'></i><a href="branchPatients.php">Patient Records</a>
         </li>
-        <li class="<?php echo ($current_page == 'stock') ? 'active' : ''; ?>">
+        <li>
             <i class='bx bx-cabinet'></i><a href="branchStock.php">Stock / Inventory</a>
         </li>
-        <li class="<?php echo ($current_page == 'analytics') ? 'active' : ''; ?>">
+        <li>
             <i class='bx bx-line-chart'></i><a href="branchAnalytics.php">Analytics</a>
         </li>
-        <li class="<?php echo ($current_page == 'approvals') ? 'active' : ''; ?>">
+        <li>
             <i class='bx bxs-check-circle'></i><a href="branchApprovals.php">Approvals</a>
         </li>
         <li class="dropdown <?php echo ($current_page == 'forms') ? 'active' : ''; ?>">
@@ -29,10 +29,10 @@
                 <li><a href="branchReferral.php">Referral Form</a></li>
             </ul>
         </li>
-        <li class="<?php echo ($current_page == 'referral_history') ? 'active' : ''; ?>">
+        <li>
             <i class='bx bx-history'></i><a href="branchReferralHistory.php">Referral History</a>
         </li>
-        <li class="<?php echo ($current_page == 'settings') ? 'active' : ''; ?>">
+        <li>
             <i class='bx bxs-cog'></i><a href="branchSettings.php">Settings</a>
         </li>
     </div>
